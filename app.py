2b1ab81f5c1fc7b24dc176543e23c938c17de34d
zackhummel <zh9h5@umsystem.edu>
2026-04-01 16:49:44 -0500

    testing user input

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ def user_input():
     time_series = input("Which time series function: ")
     beginning_date = input("Beginning date: ")
     end_date = input("End date: ")
-    return
+    return stock_symbol, chart, time_series, beginning_date, end_date
 def main():
-    user_input()
+    stock_symbol, chart, time_series, beginning_date, end_date = user_input()
+
+    print("User Inputs:")
+    print("Stock Symbol:", stock_symbol)
+    print("Chart:", chart)
+    print("Time Series:", time_series)
+    print("Beginning Date:", beginning_date)
+    print("End Date:", end_date)
