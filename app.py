9af5f1493ce70f95993cb316ea255a10c424aa25
zackhummel <zh9h5@umsystem.edu>
2026-04-01 16:40:35 -0500

    user input function

---
+++ app.py
@@ -0,0 +1,6 @@
+def user_input():
+    stock_symbol = input("Stock Symbol: ")
+    chart = input("Which chart would you like: ")
+    time_series = input("Which time series function: ")
+    beginning_date = input("Beginning date: ")
+    end_date = input("End date: ")
